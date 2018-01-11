4aac637eb90967a39b85cbe8e36b47955a116a87
cposch <cgposch@gmail.com>
2018-01-11 01:16:59 +0100

    First steps and Layout

---
+++ Coding Dojo 5/ViewModel/ItemVm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Coding_Dojo_5.ViewModel
+{
+    public class ItemVm
+    {
+        public ObservableCollection<ItemVm> ItemList { get; set; }
+        public string Description { get; set; }
+        public BitmapImage Image { get; set; }
+        public string AgeRecommendation { get; set; }
+
+        public ItemVm(string description, BitmapImage image, string ageRecommendation)
+        {
+            Description = description;
+            Image = image;
+            AgeRecommendation = ageRecommendation;
+        }
+
+        public void AddItem(ItemVm item)
+        {
+            if (ItemList == null)
+                ItemList = new ObservableCollection<ItemVm>();
+
+            ItemList.Add(item);
+        }
+    }
+}
---
+++ Coding Dojo 5/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+using GalaSoft.MvvmLight;
+
+namespace Coding_Dojo_5.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
